421ba878abdbbc018790f1c84c7ff415c83b61e3
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-05-25 16:08:32 +0300

    added new_func fixed tests

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -2,13 +2,13 @@ import pytest
 from utils import dicts
 
 
-@pytest.fixture
-def data():
-    return {"vcs": "mercurial"}
+# @pytest.fixture
+# def data():
+#     return {"vcs": "mercurial"}
 
 
 def test_get_val():
-    assert dicts.get_val(data(), "vcs", 'git') == "mercurial"
-    assert dicts.get_val(data(), "vcs") == "mercurial"
-    assert dicts.get_val({}, "vcs", 'git') == 'git'
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs", 'git') == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert dicts.get_val({}, "vcs") == 'git'
     assert dicts.get_val({}, "vcs", 'Pupa') == 'Pupa'
